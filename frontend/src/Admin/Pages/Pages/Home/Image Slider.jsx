071dec6c3a2cb3e651f1bc456536021f8948274a
sumit <zk2717162@gmail.com>
2025-07-23 14:36:05 +0530

    #

--- frontend/src/Admin/Pages/Pages/Home/Image Slider.jsx
+++ frontend/src/Admin/Pages/Pages/Home/Image Slider.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 
 const ImageSlider = () => {
-<<<<<<<<< Temporary merge branch 1
     return (
         <div>
             <h1>This is the Image slider page in which we can edit, change and delete in Image slider page</h1>
         </div>
     );
-||||||||| 0fb5881
-    return <div>ImageSlider</div>;
-=========
-    return <div className="">ImageSlider</div>;
->>>>>>>>> Temporary merge branch 2
 };
 
 export default ImageSlider;
